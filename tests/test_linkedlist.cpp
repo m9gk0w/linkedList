b3cb36b89c1208fcf95457975617e6b0ce963c96
Иван Мягков <ivanmagkov@MacBook-Air-Ivan.local>
2026-03-06 11:22:10 -0800

    First commit

---
+++ tests/test_linkedlist.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "linkedlist.h"
+
+TEST(LinkedListTest, CreateObject) {
+    linkedlist list;   // имя класса с маленькой буквы, как в заголовке
+    SUCCEED();
+}
+
+TEST(LinkedListTest, Placeholder) {
+    EXPECT_TRUE(true);
+}
